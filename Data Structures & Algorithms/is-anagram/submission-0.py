0f1c1f920e39db4f78ca3a9ef66831dac2feb78c
Ali Asad <aliasad059@gmail.com>
2026-08-12 00:06:24 -0400

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,8 @@
+from collections import Counter
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_dict = dict(Counter(s))
+        t_dict = dict(Counter(t))
+        
+        return s_dict == t_dict

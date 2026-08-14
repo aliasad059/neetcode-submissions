e30d36ddd8ae4e988a73b59a31f146c70fbe8449
Ali Asad <aliasad059@gmail.com>
2026-08-13 21:37:33 -0400

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = "".join(char for char in s.lower() if char.isalnum())
+        n=len(s)
+        for i in range(n//2): 
+            if s[i] != s[n-i-1]: 
+                return False
+        
+        return True
+
+
+
+        
